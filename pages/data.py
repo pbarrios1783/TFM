9a5a0ba58c94ee905f812e9e5b2c97d29da07da0
Patricia <79313546+pbarrios1783@users.noreply.github.com>
2024-10-07 10:08:37 +0200

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -1,7 +1,8 @@
+# Importamos las librerías
 import streamlit as st
 import pandas as pd
 
-# Cargar los datos por separado
+# Cargamos los datos por separado
 df_impuestos = pd.read_excel("./data/detalle/detalle_impuestos_propios.xlsx")
 df_cpd = pd.read_excel("./data/detalle/detalle_centros_datos.xlsx")
 df_aceleradoras = pd.read_excel("./data/detalle/detalle_aceleradoras.xlsx")
@@ -11,7 +12,7 @@ df_incubadoras = pd.read_excel("./data/detalle/detalle_incubadoras.xlsx")
 df_parques_tecnologicos = pd.read_excel("./data/detalle/detalle_parques_tecnologicos.xlsx")
 
 
-# Diccionario para mapear tipos de datos con DataFrames
+# Creamos un diccionario para mapear tipos de datos con DataFrames
 df_map = {
     "Impuestos Propios": df_impuestos,
     "Centros de Datos": df_cpd,
@@ -22,7 +23,7 @@ df_map = {
     "Parques Tecnológicos": df_parques_tecnologicos,  
 }
 
-# Diccionario que asocia el tipo de datos con las columnas relevantes
+# Creamos un diccionario que asocia el tipo de datos con las columnas relevantes
 columns_map = {
     "Impuestos Propios": ['Nombre de Impuestos'],  
     "Centros de Datos": ['Nombre', 'Tipo', 'Provincia'],
@@ -34,25 +35,26 @@ columns_map = {
  
 }
 
+# Mostramos la página en el app
 def show_page():
     # Título de la página
     st.title("Datos de España: Comunidades Autónomas")
     st.write("Detalle sólo disponible para variables de impuestos propios, centros de datos, aceleradoras, subsidios, centros tecnológicos, incubadoras y parques tecnológicos")
     
-    # Selección del tipo de datos que quiere ver el usuario
+    # Creamos el menu de selección para el tipo de datos que quiere ver el usuario
     tipo_datos = st.selectbox("Selecciona el tipo de datos que quieres ver", df_map.keys())
     
-    # Seleccionar comunidad en una lista desplegable
+    # Selecciona la comunidad en una lista desplegable
     df_seleccionado = df_map[tipo_datos]
     comunidad_seleccionada = st.selectbox("Selecciona una Comunidad Autónoma para ver detalles", df_seleccionado['Comunidad Autónoma'].unique())
     
-    # Filtrar los datos según la comunidad seleccionada
+    # Filtra los datos según la comunidad seleccionada
     df_filtrado = df_seleccionado[df_seleccionado['Comunidad Autónoma'] == comunidad_seleccionada]
     
-    # Filtrar las columnas importantes según el tipo de datos
+    # Filtra las columnas importantes según el tipo de datos
     columnas_relevantes = columns_map[tipo_datos]
     df_filtrado = df_filtrado[columnas_relevantes]
     
-   # Mostrar el DataFrame filtrado
+   # Muestra el DataFrame filtrado
     st.write(f"Mostrando datos para {comunidad_seleccionada} (Tipo de datos: {tipo_datos})")
     st.dataframe(df_filtrado)
