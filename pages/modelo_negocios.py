8d1252f7fda47c0321a801b847ab26f1487e567a
Patricia <79313546+pbarrios1783@users.noreply.github.com>
2024-10-07 12:23:14 +0200

    Update modelo_negocios.py

--- pages/modelo_negocios.py
+++ pages/modelo_negocios.py
@@ -8,16 +8,17 @@ from wordcloud import STOPWORDS
 from safetensors.torch import load_file
 import os
 import boto3
-from dotenv import load_dotenv
 
-# Cargamos las variables de entorno desde el archivo .env
-load_dotenv()
+# Accedemos a las credenciales desde los secretos de Streamlit
+aws_access_key_id = st.secrets["AWS_ACCESS_KEY_ID"]
+aws_secret_access_key = st.secrets["AWS_SECRET_ACCESS_KEY"]
+region_name = st.secrets["AWS_DEFAULT_REGION"]
 
-# Configuramos cliente de S3
+# Configuramos cliente de S3 usando los secretos
 s3 = boto3.client('s3',
-                  aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                  region_name=os.getenv('AWS_DEFAULT_REGION'))
+                  aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key,
+                  region_name=region_name)
 
 # Nombre del bucket y nombre del archivo
 BUCKET_NAME = 'tfm-modelo'
